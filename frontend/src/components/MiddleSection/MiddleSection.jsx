04d68ccbc68d86f04a2dec980ced64ecb3873857
gehadmenhem <gehad_menhem@outlook.com>
2026-01-02 14:33:12 -0700

    adding video

--- frontend/src/components/MiddleSection/MiddleSection.jsx
+++ frontend/src/components/MiddleSection/MiddleSection.jsx
@@ -4,7 +4,7 @@ export default function MiddleSection() {
   return (
     <div className="hero-container">
       {/* <video src="/videos/service_video.mp4" autoPlay loop muted /> */}
-      <video src="/videos/video2.mp4" autoPlay loop muted />
+      <video src="/videos/daycare.mp4" autoPlay loop muted />
       {/* <h1>Adventure</h1>
       <p>what are you waiting for</p> */}
     </div>
