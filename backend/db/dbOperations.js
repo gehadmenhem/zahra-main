2777c8163097a58ab95f24a644aa63cd2616e557
gehadmenhem <gehad_menhem@outlook.com>
2026-01-03 17:03:29 -0700

    prettier fix

--- backend/db/dbOperations.js
+++ backend/db/dbOperations.js
@@ -1,32 +1,31 @@
-const knexInstance = require("./dbConfig");
+/** @format */
 
-async function registerInventory(registerInfo,base64Images) {
+const knexInstance = require('./dbConfig');
+
+async function registerInventory(registerInfo, base64Images) {
   try {
     if (!registerInfo) {
-      throw new Error("registration info is required to register");
+      throw new Error('registration info is required to register');
     }
-    const result = await knexInstance("inventory.inventory").insert(
-      registerInfo
-    ).returning('vin_number');
-    if (result && result.length>=1) {
-
+    const result = await knexInstance('inventory.inventory')
+      .insert(registerInfo)
+      .returning('vin_number');
+    if (result && result.length >= 1) {
       try {
         const imagesToInsert = base64Images.map((base64Str) => ({
-    vin_number: result[0].vin_number,
-    image: base64Str,
-  }));
-    await knexInstance.transaction(async (trx) => {
-      await trx('inventory.images').insert(imagesToInsert);
-    });
-   
-  } catch (error) {
-    console.error('Error inserting images:', error);
-    throw error;
-  }
-    
+          vin_number: result[0].vin_number,
+          image: base64Str,
+        }));
+        await knexInstance.transaction(async (trx) => {
+          await trx('inventory.images').insert(imagesToInsert);
+        });
+      } catch (error) {
+        console.error('Error inserting images:', error);
+        throw error;
+      }
     }
-   
-     return('Data inserted successfully');
+
+    return 'Data inserted successfully';
   } catch (error) {
     // console.log(error);
     throw new Error(error);
@@ -36,9 +35,9 @@ async function registerInventory(registerInfo,base64Images) {
 async function getUser() {
   try {
     const result = await knexInstance
-      .select("*")
-      .from("reg.register")
-      .where("first_name", "=", "gehad");
+      .select('*')
+      .from('reg.register')
+      .where('first_name', '=', 'gehad');
 
     return result;
   } catch (error) {
@@ -47,56 +46,56 @@ async function getUser() {
   }
 }
 
-async function getInventory(){
+async function getInventory() {
   try {
-    const result = await knexInstance.select("i.*",
-    knexInstance("inventory.images")
-      .count("*")
-      .whereRaw("images.vin_number = i.vin_number")
-      .as("image_count"),
-    knexInstance("inventory.images")
-      .select("image")
-      .whereRaw("inventory.images.vin_number = i.vin_number")
-      .orderBy("id", "asc")
-      .limit(1)
-      .as("first_image")
-  )
-  .from("inventory.inventory as i");
+    const result = await knexInstance
+      .select(
+        'i.*',
+        knexInstance('inventory.images')
+          .count('*')
+          .whereRaw('images.vin_number = i.vin_number')
+          .as('image_count'),
+        knexInstance('inventory.images')
+          .select('image')
+          .whereRaw('inventory.images.vin_number = i.vin_number')
+          .orderBy('id', 'asc')
+          .limit(1)
+          .as('first_image')
+      )
+      .from('inventory.inventory as i');
 
-console.log(result)
+    console.log(result);
     return result || [];
-    
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 }
 
-async function getInventoryImages(vin_number){
+async function getInventoryImages(vin_number) {
   try {
-   
-    if(!vin_number){
-      throw new Error("validation error:vin_number is required to get the car images")
+    if (!vin_number) {
+      throw new Error(
+        'validation error:vin_number is required to get the car images'
+      );
     }
-     const result = await knexInstance
-      .select("*")
-      .from("inventory.images")
-      .where(vin_number)
-      
+    const result = await knexInstance
+      .select('*')
+      .from('inventory.images')
+      .where(vin_number);
 
     return result || [];
-    
   } catch (error) {
-    console.log(error)
-    throw new Error(error)
+    console.log(error);
+    throw new Error(error);
   }
 }
 
 async function getUserByEmail() {
   try {
     const result = await knexInstance
-      .select("*")
-      .from("reg.register")
-      .where("first_name", "=", "gehad");
+      .select('*')
+      .from('reg.register')
+      .where('first_name', '=', 'gehad');
 
     return result;
   } catch (error) {
@@ -110,9 +109,7 @@ async function registerUser(profile) {
     console.log(profile);
 
     const [email_address] = await knexInstance.transaction(async (trx) => {
-      return await trx('dbo.parent')
-        .insert(profile)
-        .returning('email_address');
+      return await trx('dbo.parent').insert(profile).returning('email_address');
     });
 
     return email_address;
@@ -129,12 +126,12 @@ async function registerUser(profile) {
 
 async function loginUser(email_address) {
   try {
-   const loginResult = await knexInstance("dbo.parent")
-      .select("*")
-      .where( email_address )
-     .first();
-   
-    return loginResult || {}
+    const loginResult = await knexInstance('dbo.parent')
+      .select('*')
+      .where(email_address)
+      .first();
+
+    return loginResult || {};
   } catch (error) {
     // PostgreSQL unique constraint error
     if (error.code === '23505' && error.constraint === 'unique_email') {
@@ -146,43 +143,42 @@ async function loginUser(email_address) {
   }
 }
 
-
-async function getReviews(){
+async function getReviews() {
   try {
-   
-   
-     const result = await knexInstance
-      .select("*")
-      .from("services.reviews")
-      .orderBy("creation_date","asc")
-      
+    const result = await knexInstance
+      .select('*')
+      .from('services.reviews')
+      .orderBy('creation_date', 'asc');
 
     return result || [];
-    
   } catch (error) {
-    console.log(error)
-    throw new Error(error)
+    console.log(error);
+    throw new Error(error);
   }
 }
 
-
 async function insertChildren(childData) {
   try {
-    console.log(profile);
+    console.log(childData);
 
     const insertChildrenResult = await knexInstance.transaction(async (trx) => {
-      return await trx('dbo.children')
-        .insert(profile)
-        .returning('*');
+      return await trx('dbo.children').insert(childData).returning('*');
     });
 
     return insertChildrenResult;
   } catch (error) {
-   
     console.error('Register error:', error);
     throw error; // rethrow other errors
   }
 }
 
-
-module.exports = { registerInventory, getUser,getInventory,getInventoryImages,registerUser,getReviews,loginUser };
+module.exports = {
+  registerInventory,
+  getUser,
+  getInventory,
+  getInventoryImages,
+  registerUser,
+  getReviews,
+  loginUser,
+  insertChildren,
+};
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
-import "./navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import {
-  faPhone,
-  faEnvelope,
-  faMapMarkerAlt
-} from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF,faGoogle } from "@fortawesome/free-brands-svg-icons";
+import './navbar.css';
 function Navbar() {
   const [click, setClick] = useState(false);
   const menuClick = () => setClick(!click);
@@ -16,48 +12,54 @@ function Navbar() {
 
   return (
     <>
-     <div className="top-header">
- <div className="top-header-left" style={{ textAlign: "right" }}>
+      <div className="top-header">
+        <div className="top-header-left" style={{ textAlign: 'right' }}>
           <a
             href="https://www.google.com/search?q=a2z+auto+repairs&sca_esv=833ffeabca5aa1d7&sxsrf=AE3TifOdq1yf8tT7-hh7YNlFzEQantIRiA%3A1753238486040&ei=1kuAaLaiApGs0PEPt9HeiAI&gs_ssp=eJzj4tVP1zc0zCrLzTJJMskzYLRSNagwNTY3NE-ytLA0MkwyTE01tDKoSEozNDVJSk42SDWwNE42SPESSDSqUkgsLclXKEotSMwsKgYA3mUVVA&oq=a2z+auto+repairs&gs_lp=Egxnd3Mtd2l6LXNlcnAiEGEyeiBhdXRvIHJlcGFpcnMqAggAMg4QLhiABBiwAxjHARivATIIEAAYgAQYsAMyBxAAGLADGB4yBxAAGLADGB4yCRAAGLADGAgYHjIJEAAYsAMYCBgeMgkQABiwAxgIGB4yCRAAGLADGAgYHjIOEAAYgAQYsAMYhgMYigUyDhAAGIAEGLADGIYDGIoFSJcQUABYAHABeACQAQCYAVqgAVqqAQExuAEByAEAmAIBoAIFmAMAiAYBkAYKkgcBMaAHgQmyBwC4BwDCBwMyLTHIBwQ&sclient=gws-wiz-serp"
-    // href="https://www.facebook.com/p/A2Z-AUTO-Repair-100063622234813/" 
-    target="_blank" 
-    rel="noopener noreferrer"
-    style={{ color: "inherit", textDecoration: "none" }}
-  >
-    <FontAwesomeIcon icon={faGoogle} style={{ marginRight: '8px', cursor: "pointer" }} />
-  </a>
-</div>
+            // href="https://www.facebook.com/p/A2Z-AUTO-Repair-100063622234813/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: 'inherit', textDecoration: 'none' }}
+          >
+            <FontAwesomeIcon
+              icon={faGoogle}
+              style={{ marginRight: '8px', cursor: 'pointer' }}
+            />
+          </a>
+        </div>
 
-
-    <div className="top-header-right">
-  <FontAwesomeIcon icon={faPhone} style={{ marginRight: '8px',color:"white" }} />
-  <p style={{ margin: 0 }}>
-    Call Us: <a href="tel:4038191822" style={{color:"white"}}>(403) 819-1822</a>
-  </p>
-</div>
-</div>
+        <div className="top-header-right">
+          <FontAwesomeIcon
+            icon={faPhone}
+            style={{ marginRight: '8px', color: 'white' }}
+          />
+          <p style={{ margin: 0 }}>
+            Call Us:{' '}
+            <a href="tel:4038191822" style={{ color: 'white' }}>
+              (403) 819-1822
+            </a>
+          </p>
+        </div>
+      </div>
 
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          
-          <img
-  src="/images/daycarelogo.jpg"
-  alt="A2Z Logo"
-  className="a2z-logo"
-/>
-           
+            <img
+              src="/images/daycarelogo.jpg"
+              alt="A2Z Logo"
+              className="a2z-logo"
+            />
           </Link>
 
           <div className="menu-icon" onClick={menuClick}>
             <i
-              className={click ? "pi pi-times" : "pi pi-bars"}
-              style={{ color: "black" }}
+              className={click ? 'pi pi-times' : 'pi pi-bars'}
+              style={{ color: 'black' }}
             ></i>
           </div>
 
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
               <NavLink
                 to="/"
@@ -82,16 +84,18 @@ function Navbar() {
                 Inventory
               </NavLink>
             </li> */}
-               <li className="nav-item">
-  <HashLink
-    to="/#services"
-    className="nav-links"
-    scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'start' })}
-    onClick={closeMobileMenu}
-  >
-    Services
-  </HashLink>
-</li>
+            <li className="nav-item">
+              <HashLink
+                to="/#services"
+                className="nav-links"
+                scroll={(el) =>
+                  el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+                }
+                onClick={closeMobileMenu}
+              >
+                Services
+              </HashLink>
+            </li>
             <li className="nav-item">
               <NavLink
                 to="/aboutus"
@@ -110,7 +114,7 @@ function Navbar() {
                 Contact Us
               </NavLink>
             </li>
-               <li className="nav-item">
+            <li className="nav-item">
               <NavLink
                 to="/signin"
                 className="nav-links"
@@ -119,8 +123,8 @@ function Navbar() {
                 Dashboard
               </NavLink>
             </li>
- 
-              {/* <li className="nav-item">
+
+            {/* <li className="nav-item">
               <NavLink
                 to="/login"
                 className="nav-links"
